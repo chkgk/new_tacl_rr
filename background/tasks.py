3034ef14c9feaf5de59820f6242017d626d767b2
reutapel <reutapel88@gmail.com>
2021-08-04 21:03:10 +0300

    Add files via upload

---
+++ background/tasks.py
@@ -0,0 +1,20 @@
+from huey import SqliteHuey, RedisHuey
+import time
+import os
+from mcts_.mcts_main import mcts_live_simu
+
+
+# setup
+REDIS_URL = os.environ.get("REDIS_URL", None)
+if not REDIS_URL:
+    huey = SqliteHuey()
+else:
+    huey = RedisHuey(url=REDIS_URL)
+
+
+# Task definitions
+@huey.task()
+def start_mcts(df, round_number):
+    # make this task longer than it really is
+    action = mcts_live_simu(df, round_number)
+    return action
